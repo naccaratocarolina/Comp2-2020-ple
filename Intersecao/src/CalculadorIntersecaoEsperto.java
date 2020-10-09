03719aae5022299b421bea7c7b947bc4c6ef0027
naccaratocarolina <naccaratocarolina@gmail.com>
2020-10-09 11:26:20 -0300

    + Intersecao

--- Intersecao/src/CalculadorIntersecaoEsperto.java
+++ Intersecao/src/CalculadorIntersecaoEsperto.java
@@ -5,14 +5,17 @@ public class CalculadorIntersecaoEsperto extends CalculadorIntersecao<Integer> {
 
     @Override
     public int getQuantidadeElementosEmComum(List<Integer> lista1, List<Integer> lista2) {
+        //Cria um HashSet com os elementos da lista1
         HashSet<Integer> hashset1 = new HashSet<Integer>();
         for (int elementoDaLista1 : lista1) hashset1.add(elementoDaLista1);
 
-        HashSet<Integer> hashset2 = new HashSet<Integer>();
+        //Cria um HashSet que percorre a lista2 e adiciona somente elementos em comum com a lista 1
+        HashSet<Integer> hashsetIntersecao = new HashSet<Integer>();
         for(int elementoDaLista2 : lista2) {
-            if(hashset1.contains(elementoDaLista2)) hashset2.add(elementoDaLista2);
+            if(hashset1.contains(elementoDaLista2)) hashsetIntersecao.add(elementoDaLista2);
         }
 
-        return hashset2.size();
+        //Retorna a contagem de elementos do HashSet de Intersecao
+        return hashsetIntersecao.size();
     }
 }
